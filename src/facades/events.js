5c583179aca4a8344ce1669d02a66fbf667a68ed
Marx Guimaraes <fidel.guimaraes@linx3.com>
2020-06-10 10:37:05 -0400

    fix: doc facades

--- src/facades/events.js
+++ src/facades/events.js
@@ -2,6 +2,12 @@ import helpers from '../helpers';
 
 const { error } = console;
 
+/**
+ * Sends a GET request to a given url.
+ *
+ * @param {string} url - The url to use
+ * in the request.
+ */
 export async function send(url) {
   try {
     helpers.ajax({

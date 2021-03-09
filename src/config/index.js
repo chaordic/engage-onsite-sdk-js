a4d1b6488e80179e1431d52a3d83d201d625a08d
Anderson Rostirolla <anderson.rostirolla@linx3.com>
2021-03-09 11:10:56 -0300

    fix: export common configs to new file, and fix spaces

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 import development from './environment/development.json';
 import production from './environment/production.json';
 import staging from './environment/staging.json';
+import common from './environment/common.json';
 
 /**
  * Set a different environment to get request at server staging or development.
@@ -8,17 +9,22 @@ import staging from './environment/staging.json';
  * @param {string} env - Environment to set different url's get request.
  */
 function getBaseUrlByEnv(env) {
-    if (env === 'stg') {
-      return staging.onsite.baseURL;
-    } else if (env === 'dev') {
-      return development.onsite.baseURL;
-    }
-    return production.onsite.baseURL;
+  if (env === 'stg') {
+    return staging.onsite.baseURL;
+  } else if (env === 'dev') {
+    return development.onsite.baseURL;
+  }
+  return production.onsite.baseURL;
 }
 
+// Merge common configs to thrid environments.
+Object.assign(development, common);
+Object.assign(production, common);
+Object.assign(staging, common);
+
 export default {
-    development,
-    production,
-    staging,
-    getBaseUrlByEnv
+  development,
+  production,
+  staging,
+  getBaseUrlByEnv
 }
